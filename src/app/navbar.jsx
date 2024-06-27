f741f781b56773e09e0f9dac6066105c59927474
Jaye Speaks <jaye.speaks@gmail.com>
2024-06-27 09:18:09 -0400

    feat: add navbar

---
+++ src/app/navbar.jsx
@@ -0,0 +1,30 @@
+'use client'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const Navbar = () => {
+    const pathname = usePathname()
+    return (
+      <nav style={{ border: 'solid black 1px'}}>
+        <ul>
+          <li>
+            <Link 
+              href="/"
+              style={{ color: `${pathname === '/' ? 'red' : 'black'}`}}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href="/cargo"
+              style={{ color: `${pathname === '/cargo' ? 'red' : 'black'}`}}
+            >
+              Cargo
+            </Link>
+          </li>
+        </ul>
+      </nav> 
+    )
+}
+export default Navbar;
